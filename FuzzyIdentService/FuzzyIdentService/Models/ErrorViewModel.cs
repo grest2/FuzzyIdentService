6e8b8276b6157f960eb282d5b8676f755ba621f8
BlamckSmordsman <polyanaozero@gmail.com>
2021-05-08 12:10:38 +0300

    master/init
    Initial repo

---
+++ FuzzyIdentService/FuzzyIdentService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FuzzyIdentService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
