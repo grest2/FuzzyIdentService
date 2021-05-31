7b0594413af1653366784cb0fa08f048119f30f8
BlamckSmordsman <polyanaozero@gmail.com>
2021-06-01 01:57:59 +0300

    feature/fuzzy-handler
    Initial db context. Added entity framework. Added class handler fo algorithms

---
+++ FuzzyIdentService/FuzzyIdentService/Models/Context/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FuzzyIdentService.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuzzyIdentService.Models.Context
+{
+     class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+        public virtual DbSet<User> User { get; set; }
+    }
+}
---
+++ FuzzyIdentService/FuzzyIdentService/Models/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuzzyIdentService.Models.Entities
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public int MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
