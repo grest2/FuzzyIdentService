eff8d321169ac02a3f3ddebce65567aeb5f84f6e
BlamckSmordsman <polyanaozero@gmail.com>
2021-08-07 16:15:01 +0300

    feature/rest-layer
    Added db layer

---
+++ FuzzyIdentService/FuzzyIdentService/Utils/Dependency Injection/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuzzyIdentService.Utils.Dependency_Injection
+{
+    public class BaseRepository
+    {
+    }
+}
