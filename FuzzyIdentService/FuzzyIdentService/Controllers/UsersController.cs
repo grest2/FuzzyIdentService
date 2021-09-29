0d30bfb10272027e2493688880ead4feb5325463
BlamckSmordsman <polyanaozero@gmail.com>
2021-09-29 21:52:53 +0300

    feature/rest-layer
    Added post method s for handling middle- and first- names

--- FuzzyIdentService/FuzzyIdentService/Controllers/UsersController.cs
+++ FuzzyIdentService/FuzzyIdentService/Controllers/UsersController.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FuzzyIdentService.Abstractions.repo;
+using FuzzyIdentService.Fuzzy_Services;
 using FuzzyIdentService.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FuzzyIdentService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[userscontroller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
         private User user { get; set; }
         private FoneticUser f_user { get; set; }
         private IBaseRepository<FoneticUser> ContextUsers { get; set; }
+        private Dictionary<string, int> pick = new Dictionary<string, int>();
+        private FuzzyHandlerScope fHandler = new FuzzyHandlerScope();
+        private int distance = 3;
 
         [HttpGet]
         public JsonResult GetUsers(string index)
@@ -23,31 +27,37 @@ namespace FuzzyIdentService.Controllers
             return new JsonResult(ContextUsers.Get(index));
         }
         [HttpPost]
-        public JsonResult BestMatch(string index, string lastName)
+        public JsonResult BestMatchLastName(string index, string lastName)
         {
-           var users = ContextUsers.Get(index);
+            var users = ContextUsers.Get(index);
+            users.ForEach(user => pick.Add(user.FoneticLastName, fHandler
+                .BestMatch(lastName, user.FoneticLastName)));
 
-           var result = users.for
-            var fUsers = users.Join(db.FoneticUser,
-                user => user.id,
-                fUser => fUser.UserId,
-                (user, fUser) => new UserFoneticUser
-                {
-                    user = user,
-                    fUser = fUser
-                });
-            var query = fUsers.OrderBy(fUser => fUser.fUser.FoneticMiddleName)
-                .Select(fUser => fUser.fUser.FoneticMiddleName)
-                .Distinct();
+            return new JsonResult(pick.Where(user => user.Value < distance)
+                .ToDictionary(element => element.Key,
+                element => element.Value));
+        }
+        [HttpPost]
+        public JsonResult BestMatchFirstName(string index, string firstname)
+        {
+            var users = ContextUsers.Get(index);
+            users.ForEach(user => pick.Add(user.FoneticLastName, fHandler
+                .BestMatch(firstname, user.FoneticLastName)));
 
-            await query.ForEachAsync(fUser => pick.Add(fUser, fHandler.BestMatch(LastName, fUser)));
+            return new JsonResult(pick.Where(user => user.Value < distance)
+                .ToDictionary(element => element.Key,
+                element => element.Value));
+        }
+        [HttpPost]
+        public JsonResult BestMatchMiddleName(string index, string middlename)
+        {
+            var users = ContextUsers.Get(index);
+            users.ForEach(user => pick.Add(user.FoneticLastName, fHandler
+                .BestMatch(middlename, user.FoneticLastName)));
 
-            string[] matchesMiddleNames = pick.Where(element => element.Value < distance)
-                .ToDictionary(element => element.Key, element => element.Value)
-                .Keys
-                .ToArray();
-
-            fUsers = fUsers.Where(fUser => matchesMiddleNames.Contains(fUser.fUser.FoneticMiddleName));
+            return new JsonResult(pick.Where(user => user.Value < distance)
+                .ToDictionary(element => element.Key,
+                element => element.Value));
         }
     }
 }
